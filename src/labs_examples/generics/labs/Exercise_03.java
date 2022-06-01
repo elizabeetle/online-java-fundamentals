0eec4999c02ccfeb666b8bba6759750746cfbee4
elizabeetle <89229500+elizabeetle@users.noreply.github.com>
2022-05-31 23:51:47 -0400

    finished generics labs

--- src/labs_examples/generics/labs/Exercise_03.java
+++ src/labs_examples/generics/labs/Exercise_03.java
@@ -1,6 +1,7 @@
 package labs_examples.generics.labs;
 
 import javax.lang.model.element.Element;
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -21,10 +22,19 @@ import java.util.List;
  *
  */
 
+//generics and primitive types
 class Generics3{
 
     public static void main(String[] args) {
         System.out.println(doubleSum(45.254, 53));
+
+        ArrayList<String> palindromes = new ArrayList<>();
+        palindromes.add("aba");
+        palindromes.add("eiorfje");
+        palindromes.add("racecar");
+        palindromes.add("111");
+
+        System.out.println(countElements(palindromes));
 
         ArrayList<Integer> testerList = new ArrayList<>();
 
@@ -41,20 +51,48 @@ class Generics3{
         largestElement(2, 5, testerList);
     }
 
+//        1) Write a generic method that accepts two generic arguments. This generic method should only accept
+//       arguments which are sublasses of Number. The generic method must return the sum (as a double) of whatever two
+//       numbers were passed in regardless of their type.
     public static <N extends Number> double doubleSum(N num, N num2){
         return num.doubleValue() * num2.doubleValue();
     }
 
-    public static void countElements(){
+//    2) Write a generic method to count the number of elements in a "Collection" of Strings that are palindromes
+    //checks if the string is a palindrome, only returns the count of palindromes
+    public static <E> int countElements(Collection<E> collection){
+        int count = 0;
 
+        Object[] array = collection.toArray();
+
+        for(int i = 0; i < array.length; i++){
+           Object word = array[i];
+            int pointer1 = 0;
+            int pointer2 = word.toString().length() - 1;
+
+            while(pointer1 < pointer2){
+                if(word.toString().charAt(pointer1) != word.toString().charAt(pointer2)){
+                     count -= 1;
+                     break;
+                }
+                pointer1 ++;
+                pointer2 --;
+            }
+            count += 1;
+        }
+        return count;
     }
-/*
-    public static <T> void exchange(T[] array, int a, int b){
-        b = array[b];
-        array[b] = array[a];
-        array[a] = b;
+
+//    3) Write a generic method to exchange the positions of two different elements in an array.
+    public static <T> void exchange(T[] array){
+        T b;
+
+       b = array[1];
+       array[1] = array[5];
+       array[5] = b;
     }
-*/
+
+//    4) Write a generic method to find the largest element within the range (begin, end) of a list.
     public static <T extends Comparable> void largestElement(int begin, int end, List<T> list){
         T max = list.get(begin);
         for (int i = begin; i < end; i++) {
